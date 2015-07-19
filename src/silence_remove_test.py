af38b68027c478e202fa7f3cd04a3b5d2348a610
dnr2 <dnr2@cin.ufpe.br>
2015-07-18 21:40:51 -0300

    improving silence remover

--- src/silence_remove_test.py
+++ src/silence_remove_test.py
@@ -2,50 +2,102 @@ import os
 import subprocess
 import re
 
-process = None
-audio_filename = 'audio_143701482948007.wav'
-convert_command = "ffmpeg -i {0} -af silencedetect=n=0.12:d=0.5 -f null -".format(audio_filename)
-# convert_command = "ffmpeg -i {0} -af silenceremove=1:0.15:0.029:-1:0.15:0.029 out.wav".format(audio_filename)
+def silence_detect(audio_filename, noise_tolerance = 0.12, silence_duration = 0.5, max_duration = None):
+    '''
+        Detects periods of silence in the audio file.
+        
+        Args;
+            audio_filename(string) full path of the wave file.
+            noise_tolerance(double, optional) Set noise tolerance specified in amplitude ratio. Default is 0.12.
+            silence_duration(double, optional) Set silence duration until notification (default is 0.5 seconds).
+            max_duration(double, optional) output audio duration in seconds cannot be longer than this value
+            
+        Returns;
+            list with time intervals with that correspond to noise (no silence) in the audio
+    '''
+    if audio_filename == None or audio_filename == '' or not audio_filename.endswith('.wav'):
+        raise Exception('Invalid file.')
+    
+    convert_command = "ffmpeg -i {0} -af silencedetect=n={1}:d={2} -f null -"
+    convert_command = convert_command.format(audio_filename, noise_tolerance, silence_duration)
+    process = output = error = None
+    
+    try:
+        print "\n #### Running convert_command ####"
+        print convert_command
+        print ""
+        
+        process = subprocess.Popen(convert_command, stdout = subprocess.PIPE, stderr = subprocess.PIPE)
+        output, error = process.communicate()
+    except Exception, e:
+        print 'Exception when converting file. Reason: ' + unicode(e.message)
 
-print "\n #### Running convert_command ####"
-print convert_command
-print ""
-
-try:
-    process = subprocess.Popen(convert_command, stdout = subprocess.PIPE, stderr = subprocess.PIPE)
-    output, error = process.communicate()
+    if process != None:
+        process.terminate()
     
     #It seems ffmpeg is directing the output to stderr instead of stdout...
-    if error is not None:
-        print error
-        error = filter( lambda a : len(a)>0, re.compile("[ \r\n,]").split(error))
+    if error is None:
+        return []
+    else :
+        tokens = filter( lambda a : len(a)>0, re.compile("[ \r\n,]").split(error))
         
         start_audio = [0.0]
         end_audio = []
         total_time = 0 
         
-        for idx in range(0,len(error)) :
-            if error[idx] == "silence_start:" :
-                end_audio.append(float(error[idx+1]))
-            if error[idx] == "silence_end:" :
-                start_audio.append(float(error[idx+1]))
-            if error[idx] == "Duration:" :
-                total_time = float(error[idx+1].split(":")[2])
+        for idx in range(0,len(tokens)) :
+            if tokens[idx] == "silence_start:" :
+                end_audio.append(float(tokens[idx+1]))
+            if tokens[idx] == "silence_end:" :
+                start_audio.append(float(tokens[idx+1]))
+            if tokens[idx] == "Duration:" :
+                total_time = float(tokens[idx+1].split(":")[2])
                 
-        end_audio.append(total_time)
+        end_audio.append(total_time)        
         
-        print start_audio
-        print end_audio
+        #if max_duration is not None and duration > max_duration:
+            #TODO: has to cut more
         
-        for idx in range(0,len(start_audio)):
-            s = start_audio[idx]
-            d = end_audio[idx] - s
-            trim_command = "sox {0} out_audio_{1}.wav trim {2} {3}".format(audio_filename, idx+1, s, d)
+        return filter(lambda a: a[0] + 1e-2 < a[1], zip(start_audio, end_audio))
+
+def silence_remove(audio_filename, output_filename = None):
+    '''
+        Create new audio files by removing silences from the original audio file.        
+        
+        Args;
+            audio_filename(string) full path of the wave file.
+            output_filename(string, optional) The prefix name for the output file (should not have extension '.wav').
+            
+        Returns;
+            number of files created
+    '''
+    if audio_filename == None or audio_filename == '' or not audio_filename.endswith('.wav'):
+        raise Exception('Invalid file.')
+        
+    if output_filename is None:
+        output_filename = audio_filename
+        
+    intervals = silence_detect(audio_filename)    
+    for idx in range(0,len(intervals)):
+        start = intervals[idx][0]
+        end = intervals[idx][1]
+        duration = end - start        
+                        
+        trim_command = "sox {0} {1}{2}.wav trim {3} {4}"
+        trim_command = trim_command.format(audio_filename, output_filename, idx+1, start, duration)
+        
+        try:
+            print "\n #### Running trim_command ####"
+            print trim_command
+            print ""
+            
             process = subprocess.Popen(trim_command, stdout = subprocess.PIPE, stderr = subprocess.PIPE)
             output, error = process.communicate()
-            
-except Exception, e:
-    print 'Exception when converting file. Reason: ' + unicode(e.message)
+        except Exception, e:
+            print 'Exception when triming file. Reason: ' + unicode(e.message)                
 
-if process != None:
-    process.terminate()
+            
+#testing
+audio_filename = 'audio_samples\\audio_143701482948007.wav'
+out_filename = 'audio_samples\\silence'
+silence_remove(audio_filename, out_filename)
